7fc9072f4ec7d73c1ee0f5c2cfde34bfc431eafd
Adrian Tombu <adrian@otso.fr>
2019-01-20 23:35:28 +0100

    fix #5 responsive main navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../assets/img/logo-greaaat.svg'
 
-export default ({ current }) => (
-  <header className="header" role="banner">
-    <div className="wrapper">
-      <a className="logo" href="/" rel="home">
-        <img alt="Greaaat - Réseau de freelances" src={logo} width="124" height="35" />
-      </a>
+export default class Header extends Component {
+  state = {
+    showMenu: false
+  }
 
-      <button id="btn-nav"><span className="btn-nav__inner"></span></button>
+  render () {
+    const { current } = this.props
+    const { showMenu } = this.state
 
-      <nav className="nav" role="navigation">
-        <ul className="nav__list">
-          <li className={getClasses(current, 'le-reseau-de-freelances')}>
-            <Link to="/le-reseau-de-freelances">Le réseau</Link>
-          </li>
-          <li className={getClasses(current, 'freelances')}>
-            <Link to="/freelances">Les freelances</Link>
-          </li>
-          <li>
-            <a href="https://blog.greaaat.com/">Le blog</a>
-          </li>
-          <li className={getClasses(current, 'contact')}>
-            <Link to="/contact">Nous contacter</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-)
+    return (
+      <header className="header" role="banner">
+        <div className="wrapper">
+          <a className="logo" href="/" rel="home">
+            <img alt="Greaaat - Réseau de freelances" src={logo} width="124" height="35" />
+          </a>
+
+          <button
+            id="btn-nav"
+            className={showMenu ? 'btn-nav--active' : ''}
+            onClick={() => this.setState({ showMenu: !showMenu })}
+          >
+            <span className="btn-nav__inner"></span>
+          </button>
+
+          <nav className={`nav ${showMenu ? 'nav__displayed' : ''}`} role="navigation">
+            <ul className="nav__list">
+              <li className={getClasses(current, 'le-reseau-de-freelances')}>
+                <Link to="/le-reseau-de-freelances">Le réseau</Link>
+              </li>
+              <li className={getClasses(current, 'freelances')}>
+                <Link to="/freelances">Les freelances</Link>
+              </li>
+              <li>
+                <a href="https://blog.greaaat.com/">Le blog</a>
+              </li>
+              <li className={getClasses(current, 'contact')}>
+                <Link to="/contact">Nous contacter</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    )
+  }
+}
 
 const getClasses = (current, slug) => {
   let classes = ''
