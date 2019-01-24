6a2b77f56fb3b3b08053d17631b61617fd8445d4
Adrian Tombu <adrian@otso.fr>
2019-01-24 16:37:37 +0100

    Hotfix graphql warning in Page component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StaticQuery } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
 import Header from '../components/Header'
